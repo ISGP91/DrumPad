cf41cea979bbad9bfddcb6ab9b0e22069ea04ec5
Inaki Garde <igarde91@gmail.com>
2022-03-16 22:16:32 +0100

    Agregado otro kit de samples al DrumPad asignados a keywords en mayusculas. Estructurado el DrumPad en html y css. Actualizacion README.

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Maquetacion en HTML, CSS y JavaScript del DrumPad Virtual.
 
 14 de Marzo -
 Creacion de repositorio del Proyecto DrumPad Virtual.
+
+16 de Marzo - 
+Agregado otro kit de samples para keywords mayuscula, y estructurado el DrumPad en html y css.
--- javascript/drumpad.js
+++ javascript/drumpad.js
@@ -1,65 +1,153 @@
 "use strict";
 
-const crashSound = "/samples/crash.wav";
-const kickSound = "/samples/kick.wav";
-const rideSound = "/samples/ride.wav";
-const tomHighSound = "/samples/tom-high.wav";
-const tomMidSound = "/samples/tom-mid.wav";
-const hiHatOpenSound = "/samples/hihat-open.wav";
-const snareSound = "/samples/snare.wav";
-const tomLowSound = "/samples/tom-low.wav";
-const hiHatCloseSound = "/samples/hihat-close.wav";
+//samples keyswords minuscula
 
-const keySounds = (e) => {
-  if (e.key == "a" || e.key == "A") {
-    drumPadPlay(crashSound);
-  } else if (e.key == "s" || e.key == "S") {
-    drumPadPlay(kickSound);
-  } else if (e.key == "d" || e.key == "D") {
-    drumPadPlay(rideSound);
-  } else if (e.key == "f" || e.key == "F") {
-    drumPadPlay(tomHighSound);
-  } else if (e.key == "g" || e.key == "G") {
-    drumPadPlay(tomMidSound);
-  } else if (e.key == "z" || e.key == "Z") {
-    drumPadPlay(snareSound);
-  } else if (e.key == "x" || e.key == "X") {
-    drumPadPlay(hiHatOpenSound);
-  } else if (e.key == "c" || e.key == "C") {
-    drumPadPlay(hiHatCloseSound);
-  } else if (e.key == "v" || e.key == "V") {
-    drumPadPlay(tomLowSound);
+const samplesMin = [
+  "/samples/crash.wav",
+  "/samples/kick.wav",
+  "/samples/kick-2.wav",
+  "/samples/clap.wav",
+  "/samples/ride.wav",
+  "/samples/tom-high.wav",
+  "/samples/tom-mid.wav",
+  "/samples/hihat-open.wav",
+  "/samples/snare.wav",
+  "/samples/tom-low.wav",
+  "/samples/hihat-close.wav",
+];
+
+//samples keywords mayusculas
+
+const samplesMay = [
+  "/samples/SamplesMay/Ride-4.wav",
+  "/samples/SamplesMay/Crash-1.wav",
+  "/samples/SamplesMay/Perc-1.wav",
+  "/samples/SamplesMay/Clap-1.wav",
+  "/samples/SamplesMay/HiHatOpen-17.wav",
+  "/samples/SamplesMay/HiHatClose-15.wav",
+  "/samples/SamplesMay/Perc-2.wav",
+  "/samples/SamplesMay/Kick-1.wav",
+  "/samples/SamplesMay/Perc-3.wav",
+  "/samples/SamplesMay/Kick-2.wav",
+  "/samples/SamplesMay/Snare-1.wav",
+];
+
+const [
+  crashMin,
+  kick1Min,
+  kick2Min,
+  clapMin,
+  rideMin,
+  tomHigMin,
+  tomMidMin,
+  hiHatOpenMin,
+  snareMin,
+  tomLowMin,
+  hiHatCloseMin,
+] = samplesMin;
+
+const [
+  rideMay,
+  crashMay,
+  tomHighMay,
+  clapMay,
+  hiHatOpenMay,
+  hiHatCloseMay,
+  tomMidMay,
+  kick1May,
+  tomLowMay,
+  kick2May,
+  snareMay,
+] = samplesMay;
+
+const keysMin = (e) => {
+  if (e.key == "4") {
+    drumPadPlay(rideMin);
+  } else if (e.key == "r") {
+    drumPadPlay(crashMin);
+  } else if (e.key == "t") {
+    drumPadPlay(tomHigMin);
+  } else if (e.key == "y") {
+    drumPadPlay(clapMin);
+  } else if (e.key == "f") {
+    drumPadPlay(hiHatOpenMin);
+  } else if (e.key == "g") {
+    drumPadPlay(hiHatCloseMin);
+  } else if (e.key == "h") {
+    drumPadPlay(tomMidMin);
+  } else if (e.key == "v") {
+    drumPadPlay(kick1Min);
+  } else if (e.key == "b") {
+    drumPadPlay(tomLowMin);
+  } else if (e.key == "n") {
+    drumPadPlay(kick2Min);
+  } else if (e.key == "m") {
+    drumPadPlay(snareMin);
   }
 };
+
+const keysMay = (e) => {
+  if (e.key == "4") {
+    drumPadPlay(rideMay);
+  } else if (e.key == "R") {
+    drumPadPlay(crashMay);
+  } else if (e.key == "T") {
+    drumPadPlay(tomHighMay);
+  } else if (e.key == "Y") {
+    drumPadPlay(clapMay);
+  } else if (e.key == "F") {
+    drumPadPlay(hiHatOpenMay);
+  } else if (e.key == "G") {
+    drumPadPlay(hiHatCloseMay);
+  } else if (e.key == "H") {
+    drumPadPlay(tomMidMay);
+  } else if (e.key == "V") {
+    drumPadPlay(kick1May);
+  } else if (e.key == "B") {
+    drumPadPlay(tomLowMay);
+  } else if (e.key == "N") {
+    drumPadPlay(kick2May);
+  } else if (e.key == "M") {
+    drumPadPlay(snareMay);
+  }
+};
+
 const drumPadPlay = (url) => {
   const soundPlay = new Audio(url).play();
 };
 
 let playCrash = document.getElementById("crash");
-playCrash.setAttribute("onclick", "drumPadPlay(crashSound)");
+playCrash.setAttribute("onclick", "drumPadPlay(crashMin)");
 
-let playKick = document.getElementById("kick");
-playKick.setAttribute("onclick", "drumPadPlay(kickSound)");
+let playKick = document.getElementById("kick1");
+playKick.setAttribute("onclick", "drumPadPlay(kick1Min)");
+
+let playKick2 = document.getElementById("kick2");
+playKick2.setAttribute("onclick", "drumPadPlay(kick2Min)");
+
+let playClap = document.getElementById("clap");
+playClap.setAttribute("onclick", "drumPadPlay(clapMin)");
 
 let playRide = document.getElementById("ride");
-playRide.setAttribute("onclick", "drumPadPlay(rideSound)");
+playRide.setAttribute("onclick", "drumPadPlay(rideMin)");
 
 let playTomHigh = document.getElementById("tomhigh");
-playTomHigh.setAttribute("onclick", "drumPadPlay(tomHighSound)");
+playTomHigh.setAttribute("onclick", "drumPadPlay(tomHigMin)");
 
 let playTomMid = document.getElementById("tommid");
-playTomMid.setAttribute("onclick", "drumPadPlay(tomMidSound)");
+playTomMid.setAttribute("onclick", "drumPadPlay(tomMidMin)");
 
 let playSnare = document.getElementById("snare");
-playSnare.setAttribute("onclick", "drumPadPlay(snareSound)");
+playSnare.setAttribute("onclick", "drumPadPlay(snareMin)");
 
 let playHiHatOpen = document.getElementById("hihatopen");
-playHiHatOpen.setAttribute("onclick", "drumPadPlay(hiHatOpenSound)");
+playHiHatOpen.setAttribute("onclick", "drumPadPlay(hiHatOpenMin)");
 
 let playHiHatClose = document.getElementById("hihatclose");
-playHiHatClose.setAttribute("onclick", "drumPadPlay(hiHatCloseSound)");
+playHiHatClose.setAttribute("onclick", "drumPadPlay(hiHatCloseMin)");
 
 let playTomLow = document.getElementById("tomlow");
-playTomLow.setAttribute("onclick", "drumPadPlay(tomLowSound)");
+playTomLow.setAttribute("onclick", "drumPadPlay(tomLowMin)");
 
-document.addEventListener("keydown", keySounds);
+document.addEventListener("keydown", keysMin);
+document.addEventListener("keydown", keysMay);
